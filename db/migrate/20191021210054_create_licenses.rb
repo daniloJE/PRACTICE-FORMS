2734f89e369c642e714e95fe21239260f44f7fad
daniloJE <danidaniposao@gmail.com>
2019-10-21 23:58:34 +0200

    finished form, added delete link

---
+++ db/migrate/20191021210054_create_licenses.rb
@@ -0,0 +1,10 @@
+class CreateLicenses < ActiveRecord::Migration[6.0]
+  def change
+    create_table :licenses do |t|
+      t.string :number
+      t.string :state
+      t.integer :driver_id
+      t.timestamps
+    end
+  end
+end
